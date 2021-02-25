6156b0513b277b2d73e9b91e807ad5811a119733
Vasiliy Lovygin <vasiliy.lovigin@interfax.ru>
2021-02-25 16:45:58 +0300

    Домашнее задание №4

---
+++ module_4/part_1/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store', default="ru",
+                     help='Choose user language: "ru", "en-GB", "es", "fr"')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    browser.user_language = user_language
+
+    yield browser
+    browser.quit()
---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+catalog_item_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+# Locators
+add_to_basket_button_locator = (By.CSS_SELECTOR, "#add_to_basket_form button.btn-add-to-basket")
+
+
+def test_item_should_contains_add_to_basket_button(browser):
+    wait = WebDriverWait(browser, 10)
+    # Наименования названия кнопки на разных языках
+    button_text = {'ru': 'Добавить в корзину', 'en-GB': 'Add to basket',
+                   'es': 'Añadir al carrito', 'fr': 'Ajouter au panier'}
+    # Язык пользователя
+    language = browser.user_language
+    # Текст кнопки в зависимости от языка
+    add_to_basket_button_text = button_text[language]
+
+    # Открываем товар по ссылке
+    browser.get(catalog_item_link)
+
+    # Страница товара содержит кнопку добавления в корзину с корректным текстом
+    add_to_basket_button = wait.until(EC.visibility_of_element_located(add_to_basket_button_locator))
+    assert add_to_basket_button.text == add_to_basket_button_text, \
+        f"Add to basket button should contains text: {add_to_basket_button_text}"
