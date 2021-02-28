6766e40bebdc014774140a727dba76a08a5d8005
Vasiliy Lovygin <vasiliy.lovigin@interfax.ru>
2021-02-28 12:05:36 +0300

    Домашнее задание №4

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -5,21 +5,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 catalog_item_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 # Locators
-add_to_basket_button_locator = (By.CSS_SELECTOR, "#add_to_basket_form button.btn-add-to-basket")
+add_to_basket_btn_locator = (By.CSS_SELECTOR, "#add_to_basket_form button.btn-add-to-basket")
 
 
 def test_item_should_contains_add_to_basket_button(browser, user_language):
+    # Arrange
     wait = WebDriverWait(browser, 10)
-    # Наименования названия кнопки на разных языках
     button_text = {'ru': 'Добавить в корзину', 'en-GB': 'Add to basket',
                    'es': 'Añadir al carrito', 'fr': 'Ajouter au panier'}
-    # Текст кнопки в зависимости от языка
-    add_to_basket_button_text = button_text[user_language]
+    expected_btn_text = button_text[user_language]
 
-    # Открываем товар по ссылке
+    # Act
     browser.get(catalog_item_link)
 
-    # Страница товара содержит кнопку добавления в корзину с ожидаемым текстом
-    add_to_basket_button = wait.until(EC.visibility_of_element_located(add_to_basket_button_locator))
-    assert add_to_basket_button.text == add_to_basket_button_text, \
-        f"Add to basket button should contains text: {add_to_basket_button_text}"
+    # Assert
+    add_to_basket_button = wait.until(EC.visibility_of_element_located(add_to_basket_btn_locator))
+    assert add_to_basket_button.text == expected_btn_text, \
+        f"Add to basket button should contains text: {expected_btn_text}"
